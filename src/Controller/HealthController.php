d3463dfab163d571ab176832787d19f6c53561e3
ssamko0911 <ssamko091184@gmail.com>
2024-07-10 19:43:33 +0100

    HealthController created

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HealthController extends AbstractController
+{
+    #[Route('/health', name: 'health', methods: ['GET'])]
+    public function checkHealth(): JsonResponse
+    {
+        return new JsonResponse('Here we go', Response::HTTP_OK);
+    }
+}
