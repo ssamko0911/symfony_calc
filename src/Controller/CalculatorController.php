73cfa6e9494a0adefd1b8fbab5811ea7508c3146
ssamko0911 <ssamko091184@gmail.com>
2024-07-11 17:54:50 +0100

    Math signs and example of Exception implemented

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -2,22 +2,39 @@
 
 namespace App\Controller;
 
+use App\Exception\MathSignException;
 use App\Service\CalculatorService;
 use InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CalculatorController extends AbstractController
 {
-    #[Route('/calculator/{numberOne}/{numberTwo}', name: 'calculator', methods: ['GET'])]
-    public function calc(int $numberOne, int $numberTwo): JsonResponse
+    public array $mathSigns = [
+        '+' => 'add',
+        '-' => 'sub',
+        '*' => 'mul',
+        '/' => 'div',
+    ];
+
+    public function __construct(private readonly CalculatorService $calculatorService)
     {
-        $calculator = new CalculatorService();
+    }
+
+    #[Route('/calculator/{numberOne}/{numberTwo}', name: 'calculator', methods: ['GET'])]
+    public function calc(int $numberOne, int $numberTwo, Request $request): JsonResponse
+    {
+        $mathSign = $request->get('mathSign') ?? '';
         try {
-            $result = $calculator->calculate($numberOne, $numberTwo);
-        } catch (InvalidArgumentException $exception) {
+            if (!array_key_exists($mathSign, $this->mathSigns)) {
+                throw new MathSignException($mathSign);
+            }
+
+            $result = $this->calculatorService->calculate($numberOne, $numberTwo, $this->mathSigns[$mathSign]);
+        } catch (InvalidArgumentException|MathSignException $exception) {
             return new JsonResponse(['error' => $exception->getMessage()], Response::HTTP_BAD_REQUEST);
         }
         return new JsonResponse($result, Response::HTTP_OK);
