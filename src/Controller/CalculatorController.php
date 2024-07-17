334aa5d6ea8e654ed7fa78a9b8a09df2eacaa385
ssamko0911 <ssamko091184@gmail.com>
2024-07-17 19:47:31 +0100

    Refactor after the lesson

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Enum\Operation;
+use App\Request\CalculatorPayloadRequest;
 use App\Service\CalculatorService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -17,7 +18,9 @@ class CalculatorController extends AbstractController
 {
     private Serializer $serializer;
 
-    public function __construct(private readonly CalculatorService $calculatorService)
+    public function __construct(
+        private readonly CalculatorService $calculatorService
+    )
     {
         $this->serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
     }
@@ -31,42 +34,10 @@ class CalculatorController extends AbstractController
         return new JsonResponse($result, Response::HTTP_OK);
     }
 
-//    #[Route('/simple/calculator', name: 'calculator', methods: ['POST'])]
-//    public function simpleCalcNew(#[MapRequestPayload]Expression $expression): JsonResponse
-//    {
-//        return new JsonResponse($expression->operandOne);
-//    }
-
-//    #[Route('/simple/calculator', name: 'calculator', methods: ['POST'])]
-//    public function simpleCalcNew(Request $request): JsonResponse
-//    {
-//        $payLoad = json_decode($request->getContent(), true);
-//        $operandOne = $payLoad['operandOne'] ?? '';
-//        $mathSign = $payLoad['operation'] ?? '';
-//        $sign = Operation::tryFrom($mathSign);
-//
-//        if (null === $sign) {
-//            return new JsonResponse('Invalid sign', Response::HTTP_BAD_REQUEST);
-//        }
-//        // [] === $arr => how to check the array is empty;
-//
-//        return new JsonResponse($operandOne);
-//    }
-
     #[Route('/simple/calculator', name: 'calculator', methods: ['POST'])]
-    public function simpleCalcNew(Request $request): JsonResponse
+    public function simpleCalcNew(#[MapRequestPayload] CalculatorPayloadRequest $calculatorPayloadRequest): JsonResponse
     {
-        $payLoad = json_decode($request->getContent(), true);
-        $mathSign = $payLoad['operation'] ?? '';
-        $sign = Operation::tryFrom($mathSign);
-
-        if (null === $sign) {
-            return new JsonResponse('Invalid sign', Response::HTTP_BAD_REQUEST);
-        }
-        // [] === $arr => how to check the array is empty;
-
-        $result = $this->calculatorService->calculate($payLoad['operandOne'], $payLoad['operandTwo'], $sign);
-
-        return new JsonResponse($this->serializer->serialize($result, 'json'), Response::HTTP_OK);
+        $result = $this->calculatorService->calculate($calculatorPayloadRequest);
+        return new JsonResponse($result, Response::HTTP_OK);
     }
 }
--- src/Service/CalculatorService.php
+++ src/Service/CalculatorService.php
@@ -2,21 +2,22 @@
 
 namespace App\Service;
 
-use App\Entity\CalculatorResult;
 use App\Enum\Operation;
+use App\Request\CalculatorPayloadRequest;
 
 class CalculatorService
 {
-    public function calculate(int $numberOne, int $numberTwo, Operation $mathSign): CalculatorResult
+    public function calculate(CalculatorPayloadRequest $request): float
     {
-        $result = match ($mathSign) {
+        $numberOne = $request->operandOne;
+        $numberTwo = $request->operandTwo;
+        $mathSign = $request->operation;
+
+        return match ($mathSign) {
             Operation::ADD => $numberOne + $numberTwo,
             Operation::SUB => $numberOne - $numberTwo,
             Operation::MUL => $numberOne * $numberTwo,
             Operation::DIV => $numberOne / $numberTwo,
-            default => 0,
         };
-
-        return new CalculatorResult($result);
     }
 }
